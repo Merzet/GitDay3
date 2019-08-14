92fbea177dc0876a96e4e5aab67981e22a5196cf
Merzet Alip <merzet.alip@gmail.com>
2019-08-14 15:42:38 -0400

    add LoginPage and comment

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
-
     WebDriver driver;
 
     @BeforeClass
